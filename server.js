99dcb0c83413a20507e3d34b366342d6374ad8f4
Aditya Ramesha <ramesha.a@husky.neu.edu>
2016-03-07 23:20:43 -0500

    Small changes in API handling.

--- server.js
+++ server.js
@@ -16,48 +16,38 @@ app.use(cookieParser())
 app.use(express.static(__dirname + '/public'));
 
 //The Yelp API handling
-var yelp = require("node-yelp");
-var client = yelp.createClient({
-    oauth: {
-        "consumer_key": "gg8OHXBFdC5S2s3Ibx8tYA",
-        "consumer_secret": "Vw103ihOCyYzXpke4_wbOBYHfXA",
-        "token": "H88uGzz66yXEsHx-4o_Afskl8F8uejMg",
-        "token_secret": "K62YYuc56QfUZ4BLFqZOa61CwSY"
-    },
+var Yelp = require('yelp');
 
-    // Optional settings:
-    httpClient: {
-        maxSockets: 25  // ~> Default is 10
-    }
+var yelp = new Yelp({
+    consumer_key: 'gg8OHXBFdC5S2s3Ibx8tYA',
+    consumer_secret: 'Vw103ihOCyYzXpke4_wbOBYHfXA',
+    token: 'H88uGzz66yXEsHx-4o_Afskl8F8uejMg',
+    token_secret: 'K62YYuc56QfUZ4BLFqZOa61CwSY',
 });
 
+// See http://www.yelp.com/developers/documentation/v2/search_api
 app.get('/searchapi', function (req, res) {
     //Sample: http://localhost:3000/searchapi?term=cream+puffs&location=San+Francisco
     var subUrl = req.url; // here: /searchapi
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log("The received URL: " + fullUrl);
 
-    term = req.query.term;
+    qTerm = req.query.term;
     location = 'Boston,MA'; //req.query.location; //Boston
     //ll = req.query.ll;
 
-    console.log("Term/loc: "+ term +"/"+ location);
-    client.search({
-        terms: term,
-        location: location,
-        //ll: ll,
-        //sort: '2',
-        category_filter: 'food'
-
-    }).then(function (data) {
-        console.log("Yelp Working: ")
-        //console.log(data);
-        res.json(data);
-
-    }).catch(function (err) {
-        console.log("Yelp Error!");
-        res.json(err);
-    });
+    console.log("Term/loc: " + qTerm + "/" + location);
+    yelp.search({term: qTerm, location: 'Boston,MA'})
+        .then(function (data) {
+            console.log("Yelp Working: ");
+            //console.log(data);
+            res.json(data);
+        })
+        .catch(function (err) {
+            console.log("Yelp Error!");
+            console.error(err);
+            res.json(err);
+        });
 });
 
 app.get('/businessapi', function (req, res) {
@@ -66,24 +56,25 @@ app.get('/businessapi', function (req, res) {
 
     businessID = req.query.biz;
 
-    console.log("Biz: "+ businessID);
-    client.business(businessID, {
-        cc: "US"
-    }).then(function (data) {
-        console.log("Yelp Working: ")
-        //console.log(data);
-        res.json(data);
+    console.log("Biz: " + businessID);
 
-    }).catch(function (err) {
-        console.log("Yelp Error!");
-        res.json(err);
+    yelp.business(businessID)
+        .then(function (data) {
+            console.log("Yelp Working: ");
+            //console.log(data);
+            res.json(data);
+
+        })
+        .catch(function (err) {
+            console.log("Yelp Error!");
+            res.json(err);
+        });
+});
+
+    app.get('/hello', function(req, res){
+        res.send('hello world');
     });
-});
 
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
+    require("./public/project/server/app.js")(app);
 
-require("./public/project/server/app.js")(app);
-
-app.listen(port, ipaddress);
+    app.listen(port, ipaddress);
